8a728240deec8782c607188e4e373d6b5cb82a90
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-11 14:54:48 +0200

    Update azure-simple-vm.tf

--- azure-simple-vm.tf
+++ azure-simple-vm.tf
@@ -1,6 +1,10 @@
 
 provider "azurerm" {
   version = "=2.0.0"
+  #subscription_id = "${var.subscriptionId}"
+  #client_id       = "${var.clientId}"
+  #client_secret   = "${var.clientSecret}"
+  #tenant_id       = "${var.tenantId}"
   subscription_id = "${var.subscriptionId}"
   client_id       = "${var.clientId}"
   client_secret   = "${var.clientSecret}"
